4ef971acf4f33c17d821ecf959689726fafb3a35
Will Coburn <willcc2002@gmail.com>
2025-09-15 21:08:55 -0400

    Initial Commit

---
+++ ReMind/ReMindApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReMindApp.swift
+//  ReMind
+//
+//  Created by Will Coburn on 9/15/25.
+//
+
+import SwiftUI
+
+@main
+struct ReMindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
