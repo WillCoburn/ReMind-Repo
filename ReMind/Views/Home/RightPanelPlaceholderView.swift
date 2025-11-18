25c205ec67de4dc2a37163f86f1aec7118f60552
WillCoburn <80481129+WillCoburn@users.noreply.github.com>
2025-11-17 22:19:15 -0500

    Apply refreshed teal background

--- ReMind/Views/Home/RightPanelPlaceholderView.swift
+++ ReMind/Views/Home/RightPanelPlaceholderView.swift
@@ -12,11 +12,28 @@ struct RightPanelPlaceholderView: View {
         }
         .padding()
         .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+        .background(
+            Color(hex: "#65cfc1")
+                .ignoresSafeArea()
+        )
     }
 }
 
 struct RightPanelPlaceholderView_Previews: PreviewProvider {
     static var previews: some View {
         RightPanelPlaceholderView()
+    }
+}
+
+extension Color {
+    init(hex: String) {
+        let scanner = Scanner(string: hex)
+        _ = scanner.scanString("#")
+        var rgb: UInt64 = 0
+        scanner.scanHexInt64(&rgb)
+        let r = Double((rgb >> 16) & 0xFF) / 255
+        let g = Double((rgb >> 8) & 0xFF) / 255
+        let b = Double(rgb & 0xFF) / 255
+        self.init(red: r, green: g, blue: b)
     }
 }
